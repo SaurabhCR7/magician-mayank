f737072357661ce732beccfa3083206952b02f2f
Saurabh <saurabhc006@gmail.com>
2019-12-19 23:58:08 +0530

    Qoutes completed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Navbar from './components/navbar/navbar';
 import Home from './components/home/home';
 import About from './components/about/about';
 import Quote from './components/quote/quote';
+import Background1 from './components/quote/images/background1.png';
+import Background2 from './components/quote/images/background2.png';
+import Background3 from './components/quote/images/background3.jpg';
+import Background4 from './components/quote/images/background4.png';
+import Background5 from './components/quote/images/background5.png';
 
 function App() {
 	return (
@@ -12,9 +17,16 @@ function App() {
 			<Navbar />
 			<Home />
 			<About />
-			<br /><br /><br /><br /><br /><br /><br /><br />
-			<Quote text={'" Magic is believing in yourself, if you can do that, you can make anything happen. "'} />
-			<br /><br /><br /><br /><br /><br /><br /><br />
+			<Quote text={'" Magic is believing in yourself, if you can do that, you can make anything happen. "'} img={Background1}/>
+			
+			<Quote text={'" Illusion is all about creating a moment of wonder & Astonishment. "'} img={Background2}/>
+			
+			<Quote text={'" Those who do not believe in Magic will never find it. "'} img={Background3}/>
+
+			<Quote text={'" Magic becomes Art when it has nothing to hide. "'} img={Background4}/>
+
+			<Quote text={'" The #1 Sleight of Hand Artist in the World. "'} img={Background5}/>
+			
 		</div>
 	);
 }
--- src/components/quote/quote.js
+++ src/components/quote/quote.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './css/quote.css';
+import Background1 from './images/background1.png';
+import Background2 from './images/background2.png';
+import Background3 from './images/background3.jpg';
+import Background4 from './images/background4.png';
+import Background5 from './images/background5.png';
 
 function Quote(props) {
 	return (
-		<div className="quote-container">
+	<div>
+		<br /><br /><br /><br /><br /><br /><br /><br />
+			<div className="quote-container" style={{backgroundImage: `url(${props.img})`}}>
 			<p className="quote-text">{props.text}</p>
 		</div>
+		<br /><br /><br /><br /><br /><br /><br /><br />
+	</div>
+		
 	);
 }
 
