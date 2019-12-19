5fc50ad716d149b417ccc74d160fb54d341f2afc
Saurabh <saurabhc006@gmail.com>
2019-12-19 14:46:15 +0530

    Test commit

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ import Navbar from "./components/navbar/navbar";
 import Home from "./components/home/home";
 
 function App() {
-  return <div className="App">
-    <Navbar />
-    <Home />
-  </div>;
+  return (
+		<div className="App">
+			<Navbar />
+			<br />
+			<br />
+			<br />
+			<Home />
+		</div>
+	);
 }
 
 export default App;
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -10,31 +10,11 @@ function Home() {
 	return (
 		<div className="home-main-container">
 			<div className="home-logo-container">
-				<img
-					src={logo}
-					width="200px"
-					height="150px"
-					data-aos="zoom-in"
-					data-aos-duration="1000"
-				/>
-				<p
-					className="logo-text"
-					data-aos="zoom-in-up"
-					data-aos-offset="300"
-					data-aos-delay="50"
-					data-aos-duration="1000"
-				>
+				<img src={logo} width="200px" height="150px" />
+				<p className="logo-text">
 					MAYANK <span>SHEKHAR</span>
 				</p>
-				<p
-					className="sublogo-text"
-					data-aos="zoom-out-up"
-					data-aos-offset="50"
-					data-aos-delay="70"
-					data-aos-duration="1000"
-				>
-					CLOSE-UP MAGICIAN
-				</p>
+				<p className="sublogo-text">CLOSE-UP MAGICIAN</p>
 			</div>
 			<div className="home-container">
 				<p className="home-text" data-aos="fade-up">
