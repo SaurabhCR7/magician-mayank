a81fe030489b5579047d2a2741225060c83ed608
Saurabh <saurabhc006@gmail.com>
2019-12-21 13:11:41 +0530

    Designed mobile view for contact

--- src/components/quote/quote.js
+++ src/components/quote/quote.js
@@ -3,14 +3,12 @@ import './css/quote.css';
 
 function Quote(props) {
 	return (
-	<div>
-		<br /><br /><br /><br /><br /><br />
-			<div className="quote-container" style={{backgroundImage: `url(${props.img})`}}>
-			<p className="quote-text">{props.text}</p>
-		</div>
-		<br /><br /><br /><br /><br /><br />
-	</div>
-		
+			<div
+				className="quote-container"
+				style={{ backgroundImage: `url(${props.img})` }}
+			>
+				<p className="quote-text">{props.text}</p>
+			</div>
 	);
 }
 
