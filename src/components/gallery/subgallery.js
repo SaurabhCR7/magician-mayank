ea7c34fd254848c4d8f3f6eb85af241656a8b048
Saurabh <saurabhc006@gmail.com>
2019-12-21 17:11:54 +0530

    Fixed subgallery overlay

--- src/components/gallery/subgallery.js
+++ src/components/gallery/subgallery.js
@@ -2,12 +2,12 @@ import React,{useState} from 'react';
 import './css/subgallery.css';
 
 function SubGallery(props) {
-  const [show, setShow] = useState("70px");
+  const [show, setShow] = useState("200px");
   const showOverlay = () => {
     setShow("0");
   }
   const hideOverlay = () => {
-    setShow("70px");
+    setShow("200px");
   }
   return (
     <div className="subgallery-container" style={{ background: `url(${props.image})`, backgroundSize: "cover" }} onMouseEnter={showOverlay} onMouseLeave={hideOverlay} >
