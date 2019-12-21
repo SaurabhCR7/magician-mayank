faff124a46be0984de0c5cc96edc711e8e3b445d
Saurabh <saurabhc006@gmail.com>
2019-12-21 11:03:06 +0530

    Configured form submission

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { SnackbarProvider } from 'notistack';
 import './App.css';
 
 import Navbar from './components/navbar/navbar';
@@ -25,16 +26,39 @@ function App() {
 			<Navbar />
 			<Home />
 			<About />
-			<Quote text={'" Magic is believing in yourself, if you can do that, you can make anything happen. "'} img={Background1}/>
+			<Quote
+				text={
+					'" Magic is believing in yourself, if you can do that, you can make anything happen. "'
+				}
+				img={Background1}
+			/>
 			<Videos />
-			<Quote text={'" Illusion is all about creating a moment of wonder & Astonishment. "'} img={Background2}/>
+			<Quote
+				text={
+					'" Illusion is all about creating a moment of wonder & Astonishment. "'
+				}
+				img={Background2}
+			/>
 			<Gallery />
-			<Quote text={'" Those  who  do  not  believe  in  Magic  will  never  find  it. "'} img={Background3}/>
+			<Quote
+				text={
+					'" Those  who  do  not  believe  in  Magic  will  never  find  it. "'
+				}
+				img={Background3}
+			/>
 			<Testimonials />
-			<Quote text={'" Magic becomes Art when it has nothing to hide. "'} img={Background4}/>
+			<Quote
+				text={'" Magic becomes Art when it has nothing to hide. "'}
+				img={Background4}
+			/>
 			<Contact />
-			<Quote text={'" The #1 Sleight of Hand Artist in the World. "'} img={Background5}/>
-			<Book />
+			<Quote
+				text={'" The #1 Sleight of Hand Artist in the World. "'}
+				img={Background5}
+			/>
+			<SnackbarProvider maxSnack={3}>
+				<Book />
+			</SnackbarProvider>
 			<Social />
 			<Footer />
 		</div>
--- src/components/book/book.js
+++ src/components/book/book.js
@@ -1,28 +1,143 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { withSnackbar } from 'notistack';
 import './css/book.css';
 
-function Book() {
-  return (
-    <div className="book-container" id="book">
-      <p className="book-heading" data-aos="fade-up" >BOOK FOR A SHOW</p>
-      <br /><br /><br /><br /><br />
-      <form>
-        <div className="book-subform">
-          <input type="text" name="name" placeholder="Name*" data-aos="fade-right"/>
-          <input type="number" name="phone" placeholder="Phone" data-aos="fade-left"/>
-        </div>
-        <div className="book-subform">
-          <input type="email" name="email" placeholder="Email*" data-aos="fade-right"/>
-          <input type="url" name="website" placeholder="Website" data-aos="fade-left"/>
-        </div>
-        <div className="book-subform">
-          <input type="text" name="subject" placeholder="Subject*" style={{ width: "100%" }} data-aos="fade-up"/>
-        </div>
-        <textarea name="describe" rows="4" cols="30" placeholder="Describe Your Project*" data-aos="fade-up"></textarea>
-        <input type="submit" value="SUBMIT" className="book-submit-btn" data-aos="fade-up"/>
-      </form>
-    </div>
-  );
+function Book(props) {
+	const [name, setName] = useState('');
+	const [phone, setPhone] = useState('');
+	const [email, setEmail] = useState('');
+	const [website, setWebsite] = useState('');
+	const [subject, setSubject] = useState('');
+	const [description, setDescription] = useState('');
+
+	const nameHandler = e => {
+		setName(e.target.value);
+	};
+	const phoneHandler = e => {
+		setPhone(e.target.value);
+	};
+	const emailHandler = e => {
+		setEmail(e.target.value);
+	};
+	const websiteHandler = e => {
+		setWebsite(e.target.value);
+	};
+	const subjectHandler = e => {
+		setSubject(e.target.value);
+	};
+	const descriptionHandler = e => {
+		setDescription(e.target.value);
+  };
+
+	const submitHandler = e => {
+		e.preventDefault();
+		axios
+      .post('https://mayankthemagician.herokuapp.com/', {
+				name: name,
+				phone: phone,
+				email: email,
+				website: website,
+				subject: subject,
+				description: description
+			})
+			.then(res => {
+        console.log('Mail sent!');
+        props.enqueueSnackbar(
+					'Thank You For Submitting the form!',
+					{
+						variant: 'success',
+						anchorOrigin: {
+							vertical: 'bottom',
+							horizontal: 'right'
+						}
+					}
+				);
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	};
+	return (
+		<div className="book-container" id="book">
+			<p className="book-heading" data-aos="fade-up">
+				BOOK FOR A SHOW
+			</p>
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+      <form onSubmit={submitHandler}>
+				<div className="book-subform">
+					<input
+						type="text"
+						name="name"
+						placeholder="Name*"
+						required
+						value={name}
+						data-aos="fade-right"
+						onChange={nameHandler}
+					/>
+					<input
+						type="number"
+						name="phone"
+						placeholder="Phone"
+						value={phone}
+						data-aos="fade-left"
+						onChange={phoneHandler}
+					/>
+				</div>
+				<div className="book-subform">
+					<input
+						type="email"
+						name="email"
+						placeholder="Email*"
+						required
+						value={email}
+						data-aos="fade-right"
+						onChange={emailHandler}
+					/>
+					<input
+						type="url"
+						name="website"
+						placeholder="Website"
+						value={website}
+						data-aos="fade-left"
+						onChange={websiteHandler}
+					/>
+				</div>
+				<div className="book-subform">
+					<input
+						type="text"
+						name="subject"
+						placeholder="Subject*"
+						required
+						value={subject}
+						style={{ width: '100%' }}
+						data-aos="fade-up"
+						onChange={subjectHandler}
+					/>
+				</div>
+				<textarea
+					name="describe"
+					rows="4"
+					cols="30"
+					required
+					placeholder="Describe Your Project*"
+					value={description}
+					data-aos="fade-up"
+					onChange={descriptionHandler}
+				></textarea>
+				<input
+					type="submit"
+					value="SUBMIT"
+					className="book-submit-btn"
+					data-aos="fade-up"
+				/>
+			</form>
+		</div>
+	);
 }
 
-export default Book;
+export default withSnackbar(Book);
