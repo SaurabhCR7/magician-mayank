f9d56b219a6afe6cecffef536bbe54b3154e9854
Saurabh <saurabhc006@gmail.com>
2020-01-23 23:05:55 +0530

    Added new images

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './css/home.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import logo from './images/logo.png';
 
 AOS.init();
 
@@ -10,18 +9,6 @@ function Home() {
 	return (
 		<div className="home-main-container" id="home">
 			<div className="home-container">
-				<br></br><br></br><br></br>
-				<p className="home-text" data-aos="fade-up" data-aos-delay="100" data-aos-duration="1000">
-					MAGICIAN
-				</p>
-				<p
-					className="home-subtext"
-					data-aos="fade-up"
-					data-aos-delay="150"
-					data-aos-duration="1200"
-				>
-					MAYANK
-				</p>
 				<p className="home-text" data-aos="fade-up" data-aos-delay="250" data-aos-duration="1000"> 
 					WELCOME TO
 				</p>
