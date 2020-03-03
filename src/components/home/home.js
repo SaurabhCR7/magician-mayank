b93d0b48b382bef58bda432f48d6a3f64c73c4a6
Saurabh <saurabhc006@gmail.com>
2020-03-03 22:55:54 +0530

    Added hire btn

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './css/home.css';
 import AOS from 'aos';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 import 'aos/dist/aos.css';
 
 AOS.init();
@@ -9,17 +10,25 @@ function Home() {
 	return (
 		<div className="home-main-container" id="home">
 			<div className="home-container">
-				<h1 className="home-text" data-aos="fade-up"> 
+				<h1 className="home-text" data-aos="fade-up">
 					WELCOME TO
 				</h1>
 				<h1
 					className="home-subtext"
 					data-aos="fade-up"
-					data-aos-delay="300"
-					data-aos-duration="1200"
 				>
 					THE ART OF ILLUSION
 				</h1>
+				<br></br>
+				<AnchorLink
+					href="#book"
+					className="home-link"
+					data-aos="fade-up"
+					data-aos-delay="600"
+					data-aos-duration="1000"
+				>
+					HIRE NOW
+				</AnchorLink>
 			</div>
 		</div>
 	);
